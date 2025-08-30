270eba89e29e285b3d9e2589e2e83db354dba4f4
Majkl <mpiksa@seznam.cz>
2025-08-30 16:24:07 +0200

    feat: add GetFilesInfo and GetMD5 methods

--- FolderSync/Program.cs
+++ FolderSync/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
-namespace MyConsoleApp
+namespace FolderSync
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string pathSourceFolder = args[0];
+            string pathReplicaFolder = args[1];
+            string pathLogFolder = args[2];
+            TimeSpan interval = TimeSpan.Parse(args[3]);
+            
+            Console.WriteLine($"Sync every {interval}");
+
+            var test = GetFilesInfo(pathSourceFolder);
+        }
+
+        static Dictionary<string, string>? GetFilesInfo(string folderPath)
+        {
+            try
+            {
+                Dictionary<string, string> filesDir = new Dictionary<string, string>();
+                var files = Directory.GetFiles(folderPath);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    filesDir.Add(files[i], GetMD5(files[i]));
+                }
+                return filesDir;
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"Directory {folderPath} not found. {e.Message}");
+                return null;
+            }
+        }
+
+        static string GetMD5(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var content = File.OpenRead(filePath))
+                {
+                    byte[] hashBytes = md5.ComputeHash(content);
+                    StringBuilder sb = new StringBuilder();
+                    foreach (byte hashByte in hashBytes)
+                    {
+                        sb.Append(hashByte.ToString());
+                    }
+                    return sb.ToString();
+                }
+            }
         }
     }
 }
